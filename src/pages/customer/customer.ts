f671ae3ef7108bb9129eed9ffbc4f3babdeb429f
eidanfi <eidanfi@gmail.com>
2017-09-11 13:11:07 +0300

    get geocoding

--- src/pages/customer/customer.ts
+++ src/pages/customer/customer.ts
@@ -5,7 +5,7 @@ import { LoginPage } from "../login/login";
 import { AutocompletePage } from "../autocomplete/autocomplete";
 import { HttpClient } from "@angular/common/http";
 
-
+declare var google:any;
 
 @IonicPage()
 @Component({
@@ -16,13 +16,16 @@ import { HttpClient } from "@angular/common/http";
 export class CustomerPage {
   address;
   flightNum: any;
+  flightInfo: string;
   authData:AuthProvider;
   confirmm:string= 'Do you want to logout?';
   constructor(public navCtrl: NavController, authdata:AuthProvider,
   platform: Platform,public alertCtrl: AlertController, private modalCtrl: ModalController, private http: HttpClient) {
     this.authData = authdata;
     this.address = {
-      place: ''
+      place: '',
+      latitude: '',
+      longitude: ''
     };
   }
 
@@ -31,6 +34,21 @@ export class CustomerPage {
     let me = this;
     modal.onDidDismiss(data => {
       this.address.place = data;
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': this.address.place}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          let a,b;
+          a = results[0].geometry.location.lat();
+          b = results[0].geometry.location.lng();
+          //this.address.latitude = results[0].geometry.location.lat();
+        console.log("Latitude: "+results[0].geometry.location.lat());
+        console.log("Longitude: "+results[0].geometry.location.lng());
+        } 
+  
+        else {
+          console.log("Geocode was not successful for the following reason: " + status);
+        }
+      });
     });
     modal.present();
   }
@@ -56,11 +74,13 @@ export class CustomerPage {
   }
 
 
-  testFlight(){
+  getFlightInfo(){
      // Make the HTTP request:
      this.http.get('https://taxiserver.herokuapp.com/api/load/' + this.flightNum).subscribe(data => {
       // Read the result field from the JSON response.
+      this.flightInfo = "טיסה מספר: " + this.flightNum + " ממריאה בתאריך: " + data["date"] + " בשעה: " + data["time"];
       console.log(data);
+      console.log(this.flightInfo);
     });
   }
 
